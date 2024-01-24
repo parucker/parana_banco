96dc49dd50bd79f2dc65c3c83b6bb0f27758b280
parucker <henriquewps@gmail.com>
2024-01-23 22:53:35 -0300

    Fix lint

--- locustfile.py
+++ locustfile.py
@@ -8,4 +8,8 @@ class LoadTest(HttpUser):
     @task
     def predict_without_batch(self):
         request_body = {"feature_1": 1.2, "feature_2": 2.3}
-        self.client.post("http://predict_without_batch:80/predict", json=request_body, name="predict_without_batch")
+        self.client.post(
+            "http://predict_without_batch:80/predict",
+            json=request_body,
+            name="predict_without_batch",
+        )
